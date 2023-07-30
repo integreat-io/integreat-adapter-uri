6bb9c1cdc18d6db323d1d753534afc4bdcf0caf3
Kjell-Morten <post@kjellmorten.no>
2023-07-30 22:23:08 +0200

    Implement adapter and transformer

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { prepareTemplate, replaceTemplate } from './transformer.js'
+import type { Adapter } from 'integreat'
+
+const adapter: Adapter = {
+  prepareOptions(_options, _serviceId) {
+    return {}
+  },
+
+  async normalize(action, _options) {
+    return action
+  },
+
+  async serialize(action, _options) {
+    const template = action.meta?.options?.uri
+    if (typeof template === 'string') {
+      const parts = prepareTemplate(template)
+      const uri = await replaceTemplate(parts, action)
+      return {
+        ...action,
+        meta: { ...action.meta, options: { ...action.meta?.options, uri } },
+      }
+    }
+
+    return action
+  },
+}
+
+export default adapter
